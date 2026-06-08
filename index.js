1fca48d26db4917bb70a9ead216864fdcb13dad2
Aryan8899 <aryanpandita003@gmail.com>
2026-06-08 15:48:49 +0530

    updatedk

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "https://anix-new.vercel.app",
-  "https://adminanix.vercel.app/",
+  "https://adminanix.vercel.app",
 ];
 
 app.use(
